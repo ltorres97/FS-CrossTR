620a585afb59633598edc0138160d589fb39ef05
ltorres97 <42170137+ltorres97@users.noreply.github.com>
2023-01-31 15:49:11 +0000

    Update eval_model.py

--- FS-CrossTR/eval_model.py
+++ FS-CrossTR/eval_model.py
@@ -3,33 +3,24 @@ from gnntr_eval import GNNTR_eval
 import statistics
 import matplotlib.pyplot as plt
 
-def save_ckp(state, is_best, checkpoint_dir, best_model_dir, filename, best_model):
+def save_ckp(state, is_best, checkpoint_dir, filename):
     
     f_path = checkpoint_dir + filename
     torch.save(state, f_path)
-    if is_best:
-        best_fpath = best_model_dir + best_model
-        shutil.copyfile(f_path, best_fpath)
 
-def save_result(epoch, N, exp, filename, tl):
+def save_result(epoch, N, exp, filename):
             
     file = open(filename, "a")
-    
     file.write("Results: " + "\t")
     if epoch < N:
         file.write(str(exp) + "\t")
     if epoch == N:
         file.write(str(exp))
-        if tl == 0:
-            for list_acc in exp:
-                file.write(str(epoch) + " Support Sets: (Mean:"+ str(statistics.mean(list_acc)) +", SD:" +str(statistics.stdev(list_acc)) + ") | \t")
-        elif tl == 1:
-            file.write(str(epoch) + " Support Sets: (Mean:"+ str(statistics.mean(exp[0])) +", SD:" +str(statistics.stdev(exp[0])) + ") | \t")
-            
-          
+        for list_acc in exp:
+            file.write(str(epoch) + " Support Sets: (Mean:"+ str(statistics.mean(list_acc)) +", SD:" +str(statistics.stdev(list_acc)) + ") | \t")
+        
     file.write("\n")
     file.close()
-
 
 dataset = "tox21"
 gnn= "gin"
@@ -37,24 +28,17 @@ support_set = 10
 pretrained = "pre-trained/supervised_contextpred.pth"
 baseline = 0
 device = "cuda:0"
-tl = 0
-device = "cuda:0"      
 model_eval = GNNTR_eval(dataset, gnn, support_set, pretrained, baseline, tl)
 model_eval.to(device)
 
 print("Dataset:", dataset)
 
-if tl == 0:
-    if dataset== "tox21":
-        exp = [[],[],[]]
-        labels =  ['SR-HSE', 'SR-MMP', 'SR-p53']
-    elif dataset == "sider":
-        exp = [[],[],[],[],[],[]]
-        labels =  ['R.U.D.', 'P.P.P.C.', 'E.L.D.', 'C.D.', 'N.S.D.', 'I.P.P.C.']    
-
-if tl == 1:
-   exp = [[]]
-   labels = ['Mean_Task']
+if dataset== "tox21":
+    exp = [[],[],[]]
+    labels =  ['SR-HSE', 'SR-MMP', 'SR-p53']
+elif dataset == "sider":
+    exp = [[],[],[],[],[],[]]
+    labels =  ['R.U.D.', 'P.P.P.C.', 'E.L.D.', 'C.D.', 'N.S.D.', 'I.P.P.C.']    
       
 N = 30
    
@@ -76,25 +60,16 @@ for epoch in range(1, 10000):
           index = exp[i].index(min(exp[i]))
           exp[i][index] = roc_scores[i]
     
-    #save_result(epoch, N, exp, "results-exp/mean-FS-CrossTR_tox21_10.txt", tl)
+    #save_result(epoch, N, exp, "results-exp/mean-FS-CrossTR_tox21_10.txt")
     
-    if tl == 0:
-        if dataset == "tox21":
-            box_plot_data=[exp[0], exp[1], exp[2]]
-            plot_title = "Tox21"
-        elif dataset == "sider":
-            box_plot_data=[exp[0], exp[1], exp[2], exp[3], exp[4], exp[5]]
-            plot_title = "SIDER"  
+    if dataset == "tox21":
+        box_plot_data=[exp[0], exp[1], exp[2]]
+        plot_title = "Tox21"
+    elif dataset == "sider":
+        box_plot_data=[exp[0], exp[1], exp[2], exp[3], exp[4], exp[5]]
+        plot_title = "SIDER"  
     
-    if tl == 1:
-        if dataset == "tox21":
-            box_plot_data=[exp[0]]
-            plot_title = "Transfer-Learning (SIDER->Tox21)"
-        elif dataset == "sider":
-            box_plot_data=[exp[0]]
-            plot_title = "Transfer-Learning (Tox21->SIDER)"
-
-    
+      
     fig = plt.figure()   
     fig.suptitle(plot_title, fontsize=14, fontweight='bold')
     ax = fig.add_subplot(111)
@@ -102,7 +77,7 @@ for epoch in range(1, 10000):
     ax.set_xlabel('Test Task')
     ax.set_ylabel('ROC-AUC score')
     plt.grid(b=False)
-    #if epoch == N and tl == 0 and baseline == 0:
+    #if epoch == N and baseline == 0:
      #   plt.savefig('plots/figures/boxplot_GT_'+ str(dataset) + '_' + str(support_set), dpi=300)
     #plt.savefig('plots/boxplot_GT_'+ str(dataset) + '_' + str(support_set), dpi=300)
     plt.show()
