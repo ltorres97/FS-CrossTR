5399d00679d508cea83ed83eb22b40092e33601e
ltorres97 <42170137+ltorres97@users.noreply.github.com>
2023-01-31 15:53:28 +0000

    Update train_model.py

--- FS-CrossTR/train_model.py
+++ FS-CrossTR/train_model.py
@@ -4,9 +4,6 @@ from gnntr_train import GNNTR
 def save_ckp(state, is_best, checkpoint_dir, filename):
     f_path = checkpoint_dir + filename
     torch.save(state, f_path)
-    #if is_best:
-     #   best_fpath = best_model_dir + best_model
-      #  shutil.copyfile(f_path, best_fpath)
 
 dataset = "tox21"
 gnn= "gin" #gin, graphsage, gcn
